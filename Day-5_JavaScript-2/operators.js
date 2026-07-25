f5325181e31421c1cad10ded03884576c67c4280
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-07-25 11:36:41 +0530

    push operator code part

---
+++ Day-5_JavaScript-2/operators.js
@@ -0,0 +1,108 @@
+// -------------- Operators -------------->
+
+// symbols that perform perticular functions
+
+// ----------types----------->
+
+// 1. Arithmetic   ||   Unary operator
+// 2. Assignment
+// 3. Comparison
+// 4. Logical
+// 5. Ternary 
+
+//  ----------- Arithmetic Operator ---------->
+
+// let a = 10;
+// let b = 5;
+// let c = a+b;
+// console.log(c)
+
+// let a = 10;
+// let b = 5;
+// let c = a-b;
+// console.log(c)
+
+// let a = 10;
+// let b = 5;
+// let c = a*b;
+// console.log(c)
+
+// let a = 10;
+// let b = 5;
+// let c = a/b;
+// console.log(c)
+
+// let a = 10;
+// let b = 5;
+// let c = a%b;
+// console.log(c)
+
+// let a = 10;
+// let b = 2;
+// let c = a**b;   // power
+// console.log(c)
+
+// let a = 10;
+// console.log(a)
+// a++;                 // Unary
+// console.log(a)
+// a--;
+// console.log(a)
+
+// -------------- Assignment Operators ------------>
+
+// let a = 10;         // 10 is assigned to a
+// let b = 5;
+// console.log(a)
+// a+=b;                  // a=a+b;
+// console.log(a)
+
+// ----------------Comparison Operator ---------------->
+// let a = 5;
+// let b = a;
+// if(a==b){       // a==b is true
+//     console.log("both are equal");
+// }else{
+//     console.log("not equal");
+// }
+
+// console.log(Number(true))     // 1
+// console.log(Number(false))       // 0
+
+// let a = 10
+// let b = "10"
+// console.log(a==b)        //true
+
+// let a = 10
+// let b = "10"
+// console.log(a===b)       // false
+
+// let a = 10;
+// let b = 10;
+// console.log(a>b)        // false
+
+// let a = 10;
+// let b = 10;
+// console.log(a>=b)        // true
+
+// -------------- Logical Operators --------------->
+
+// 1. and   &&
+// 2. or    ||
+// 3. not   !
+
+// let age = 20;
+// if(age>18  &&  age<=22){
+//     console.log("You are eligible to vote Modiji")
+// }else{
+//     console.log("Not eligible")
+// }
+
+let age = 20;
+if(age>181  ||  age<=22){
+    console.log("You are eligible to vote Modiji")
+}else{
+    console.log("Not eligible")
+}
+
+
