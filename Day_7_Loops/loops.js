9c858e7b5794fbb0b9396c7495aa98a3289cad24
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-07-30 12:10:23 +0530

    push loops js code

---
+++ Day_7_Loops/loops.js
@@ -0,0 +1,78 @@
+// Repeatations of a perticular task until a termination condition is achieved is looping.
+// Reduces line of codes.
+// Improves readability
+
+// Types
+ 
+// 1. for loop  --- number of iterations are known
+// 2. while loop
+// 3. do while loop
+
+//------ For loop ---------->
+// for(let i = 0; i<10; i++){
+//     console.log("Aman", i)      
+//     console.log(i)      // 0 to 9 
+// }
+// let n = 3;
+// for(let i = 1; i<=10; i++){
+//     console.log(15*i)
+//     console.log(`${n} * ${i} = ${n*i}` )
+// }
+
+//  for(let i = 1; i<=5 ; i++){
+//        let star = "*"
+//        console.log(star.repeat(i))    // left Pattern
+//     }
+
+//  for(let i = 1; i<=5 ; i++){
+//        let star = "*"
+//        let space = " " 
+//        console.log(space.repeat(5-i), star.repeat(i))     // Right pattern
+//     } 
+
+    // for(let i = 1; i<=5 ; i++){
+    //    let star = "*"
+    //    let space = " " 
+    //    console.log(space.repeat(5-i), star.repeat(i*2-1))     // Centre pattern
+    // } 
+
+// ----------- While loop ---------------->
+
+// Number of iterations are unknown
+// let i=1;
+// while(i<=5){
+//     console.log("Aman")    // 5 times Aman
+//     i++
+// }
+
+// let n = 19
+// let i = 1
+// while(i<=10){
+//     console.log(`${n} * ${i} = ${n*i}`)   // table of 19
+//     i++
+// }
+
+//------- Do while loop ----------------->
+
+// Runs atleast one time 
+
+// let i = 1
+// do {
+//     console.log("Hello")    // 10 times
+//     i++
+// } while (i<=10);
+
+// let i = 10
+// do {
+//     console.log("Hello")    // 1 time
+//     i++
+// } while (i<=10);
+
+// let i = 9
+// do {
+//     console.log("Hello")    // 2 times
+//     i++
+// } while (i<=10);
+
+// Homework 
+// print diamond pattern
